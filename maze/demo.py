9b6d72936658039a2521a873a03a191d69aca8fc
kurumi <935211204@qq.com>
2017-12-11 19:30:42 +0800

    demo

---
+++ maze/demo.py
@@ -0,0 +1,26 @@
+# _*_coding:utf-8_*_
+
+from env import Maze
+from QLearning import QLearningTable
+
+def update():
+    for episode in range(100):
+        observation = env.reset()
+        while True:
+            env.render()
+            action = RL.choose_action(str(observation))
+            observation_, reward, done = env.step(action)
+            RL.learn(str(observation), action, reward, str(observation_))
+            observation = observation_
+            if done:
+                break
+    print('game over')
+    env.destroy()
+
+
+if __name__ == "__main__":
+    env = Maze()
+    RL = QLearningTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
+    env.mainloop()
